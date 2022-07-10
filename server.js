8a808f1a1c26a9aa4abbcfb4db2d2eb2071314b5
Nikola <snstotti@gmail.com>
2022-07-10 18:57:32 +0300

    edit path in server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const middlewares = jsonServer.defaults({
 const PORT = process.env.PORT || 3000;
 server.use(middlewares);
 server.use(jsonServer.rewriter({
-  '/api/*': '/$1',
+  '/*': '/$1',
 }))
 server.use(router);
 server.listen(PORT, () => {
